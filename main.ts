a344b7ea88bd5e862ee0b7b1818b956f1f742f25
Victor <victor.choi@me.com>
2023-03-03 13:01:36 -0500

    Implemented posting feature

--- main.ts
+++ main.ts
@@ -1,14 +1,29 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Component,
+	Editor,
+	MarkdownRenderer,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	accessToken: string;
+	blogName: string;
+	tags: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	accessToken: "",
+	blogName: "",
+	tags: "",
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -17,73 +32,73 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		const ribbonIconEl = this.addRibbonIcon(
+			"dice",
+			"Sample Plugin",
+			(evt: MouseEvent) => {
+				// Called when the user clicks the icon.
+				new Notice("This is a notice!");
+			}
+		);
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText("Status Bar Text");
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+			id: "post-editor-note",
+			name: "Post the editor note",
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				const tagModal = new TagModal(
+					this.app,
+					async (tags: string) => {
+						const el = document.createElement("div");
+						await MarkdownRenderer.renderMarkdown(
+							editor.getValue(),
+							el,
+							this.app.workspace.getActiveFile()?.path ?? "/",
+							view
+						);
+
+						await postToTistory({
+							accessToken: this.settings.accessToken,
+							blogName: this.settings.blogName,
+							title: view.getDisplayText(),
+							content: el.innerHTML,
+							tags,
+						});
 					}
+				);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+				tagModal.open();
+			},
 		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+			console.log("click", evt);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(
+			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+		);
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,18 +106,38 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class TagModal extends Modal {
+	tags: string;
+	onSubmit: (tags: string) => void;
+
+	constructor(app: App, onSubmit: (tags: string) => void) {
 		super(app);
+		this.onSubmit = onSubmit;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.createEl("h1", { text: "Insert tags" });
+
+		new Setting(contentEl).setName("Tags").addText((text) =>
+			text.onChange((value) => {
+				this.tags = value;
+			})
+		);
+
+		new Setting(contentEl).addButton((btn) =>
+			btn
+				.setButtonText("Submit")
+				.setCta()
+				.onClick(() => {
+					this.close();
+					this.onSubmit(this.tags);
+				})
+		);
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,22 +151,66 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", { text: "Blog" });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Setting Access Token")
+			.setDesc(
+				"This is a secret token to post an article to tistory blog"
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.accessToken)
+					.onChange(async (value) => {
+						this.plugin.settings.accessToken = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Setting Blog Name")
+			.setDesc("This is a blog name that you want to post an article ")
+			.addText((text) => {
+				text.setPlaceholder("Enter your blog name")
+					.setValue(this.plugin.settings.blogName)
+					.onChange(async (value) => {
+						this.plugin.settings.blogName = value;
+						await this.plugin.saveSettings();
+					});
+			});
+	}
+}
+
+type PostParams = {
+	accessToken: string;
+	blogName: string;
+	title: string;
+	content: string;
+	tags?: string;
+};
+
+async function postToTistory({
+	accessToken,
+	blogName,
+	title,
+	content,
+	tags = "",
+}: PostParams) {
+	const queries = `access_token=${accessToken}&output=json&blogName=${blogName}&title=${title}&content=${encodeURIComponent(
+		content
+	)}&visibility=1&category=0&tag=${tags}`;
+	const url = `https://www.tistory.com/apis/post/write?${queries}`;
+
+	try {
+		await fetch(url, {
+			method: "POST",
+		});
+	} catch (err) {
+		console.log("error: ", err);
+		throw new Error(err);
 	}
 }
