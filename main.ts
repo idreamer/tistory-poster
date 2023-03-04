52ab3830a10065c7147e0601717abc01fa6abc80
Victor <victor.choi@me.com>
2023-03-04 06:39:59 -0500

    updated tistory meta info

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 import {
 	App,
-	Component,
 	Editor,
 	MarkdownRenderer,
 	MarkdownView,
@@ -30,22 +29,6 @@ export default class MyPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"dice",
-			"Sample Plugin",
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice("This is a notice!");
-			}
-		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
 
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
@@ -78,7 +61,6 @@ export default class MyPlugin extends Plugin {
 		});
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
